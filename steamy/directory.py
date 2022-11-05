ebd94b806792bf60eb7fa4c69d9442a103a4cef1
samicrusader <hi@samicrusader.me>
2022-11-04 20:27:28 -0400

    add: base for DirectoryServer

--- steamy/directory.py
+++ steamy/directory.py
@@ -1,77 +1,52 @@
 import socketserver
 import logging
 
-class EchoRequestHandler(socketserver.BaseRequestHandler):
+
+class DirectoryServerHandler(socketserver.BaseRequestHandler):
     def __init__(self, request, client_address, server):
-        self.logger = logging.getLogger('EchoRequestHandler')
-        self.logger.debug('__init__')
+        self.logger = logging.getLogger('DirectoryServer')
         socketserver.BaseRequestHandler.__init__(self, request, client_address, server)
         return
-
-    def setup(self):
-        self.logger.debug('setup')
-        return socketserver.BaseRequestHandler.setup(self)
 
     def handle(self):
         self.logger.debug('handle')
 
         # Echo the back to the client
-        data = self.request.recv(1024)
-        self.logger.debug('recv()->"%s"', data)
-        self.request.send(data)
+        version = int.from_bytes(self.request.recv(4), 'little')
+        self.request.send(b'\x01')  # acknowledge connection
+        length = int.from_bytes(self.request.recv(4), 'little')
+        if len(length) is not 4:
+            logging.debug(f'Invalid header from client {self.client_address[0]}')
+            self.finish()
+        message = self.request.read(length)
+        logging.debug(f'Client {self.client_address[0]} sent a version {version} command')
+
         return
 
-    def finish(self):
-        self.logger.debug('finish')
-        return socketserver.BaseRequestHandler.finish(self)
 
-
-class EchoServer(socketserver.TCPServer):
-
-    def __init__(self, server_address, handler_class=EchoRequestHandler):
-        self.logger = logging.getLogger('EchoServer')
+class DirectoryServer(socketserver.TCPServer):
+    def __init__(self, server_address, handler_class=DirectoryServerHandler):
+        self.logger = logging.getLogger('DirectoryServer')
         self.logger.debug('__init__')
         socketserver.TCPServer.__init__(self, server_address, handler_class)
         return
 
-    def server_activate(self):
-        self.logger.debug('server_activate')
-        socketserver.TCPServer.server_activate(self)
-        return
-
     def serve_forever(self):
-        self.logger.debug('waiting for request')
-        self.logger.info('Handling requests, press <Ctrl-C> to quit')
+        self.logger.info('Server is listening')
         while True:
             self.handle_request()
         return
 
-    def handle_request(self):
-        self.logger.debug('handle_request')
-        return socketserver.TCPServer.handle_request(self)
-
-    def verify_request(self, request, client_address):
-        self.logger.debug('verify_request(%s, %s)', request, client_address)
-        return socketserver.TCPServer.verify_request(self, request, client_address)
-
-    def process_request(self, request, client_address):
-        self.logger.debug('process_request(%s, %s)', request, client_address)
-        return socketserver.TCPServer.process_request(self, request, client_address)
-
     def server_close(self):
-        self.logger.debug('server_close')
+        self.logger.info('stopping server...')
         return socketserver.TCPServer.server_close(self)
 
-    def finish_request(self, request, client_address):
-        self.logger.debug('finish_request(%s, %s)', request, client_address)
-        return socketserver.TCPServer.finish_request(self, request, client_address)
-
-    def close_request(self, request_address):
-        self.logger.debug('close_request(%s)', request_address)
-        return socketserver.TCPServer.close_request(self, request_address)
+    def process_request(self, request, client_address):
+        self.logger.debug(f'Connection established from {client_address}')
+        return socketserver.TCPServer.process_request(self, request, client_address)
 
 
 def run(ip: str, port: int):
     address = (ip, port)  # let the kernel give us a port
-    server = EchoServer(address, EchoRequestHandler)
+    server = DirectoryServer(address, DirectoryServerHandler)
     server.serve_forever()
