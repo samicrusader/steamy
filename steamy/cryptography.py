54331ccb5aa4d617643f2a82867ef6576d035b42
samicrusader <hi@samicrusader.me>
2022-11-08 06:08:29 -0500

    refactor: key sending is now eased by using cryptography.sign_key_with_rsa

--- steamy/cryptography.py
+++ steamy/cryptography.py
@@ -1,5 +1,6 @@
 import hashlib
 from Crypto.PublicKey import RSA
+from Crypto.Util.number import long_to_bytes
 
 primary_key = RSA.construct((
     16972286244849771984622451367228102174501719320240653652662330551811042672286869215561928929175827402752178598753445713908601088303475414633015321035251571211566978795872136026380777563265395267739035345972002870926699475330208864762370786289338571780113370443705220053580642621034990905081765661421515793197322286229310533378663925641599451069026309189868652973825372527822995378307658254979118985781849587869182040447299629997994344818433422788110169038839718660945475305838947440389938769132300261658746306829589651102747347903636404092182462550116241824059779050479962155170547507469291673134785087525867735462771,
@@ -14,7 +15,22 @@ network_key = RSA.construct((
 ))
 
 
-def sign_message_rsa(key, message: bytes):
+def sign_key_with_rsa(key_to_sign: RSA.RsaKey, key: RSA.RsaKey):
+    data = convert_key_to_data(key_to_sign)
+    signature = sign_message_rsa(key, data)
+    return len(data).to_bytes(2, 'big') + data + len(signature).to_bytes(2, 'big') + signature
+
+
+def convert_key_to_data(key: RSA.RsaKey):
+    data = b'\x30\x81\x9d\x30\x0d\x06\x09\x2a\x86\x48\x86\xf7\x0d\x01\x01\x01\x05\x00\x03\x81\x8b\x00\x30\x81\x87\x02' \
+           b'\x81\x81\x00'
+    data += long_to_bytes(key.n)
+    data += b'\x02\x01'
+    data += long_to_bytes(key.d)
+    return data
+
+
+def sign_message_rsa(key: RSA.RsaKey, message: bytes):
     sha1 = hashlib.sha1()
     sha1.update(message)
     message_hash = sha1.digest()
